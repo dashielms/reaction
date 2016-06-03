56109afa4da4b38c99704241a1e3184e7f7388c5
dashielms <dashielms@gmail.com>
2016-06-03 06:21:44 -0400

    added dependencies in <hf>/package.js (rc:core-theme, base.less, variable.less)

--- packages/hf-freshstore/package.js
+++ packages/hf-freshstore/package.js
@@ -6,4 +6,16 @@ Package.describe({
 
 Package.onUse(function (api) {
 	api.versionsFrom("METEOR@1.3");
+
+	// Tells meteor to use the LESS precompiler.
+	// Any included LESS files must appear below this use() call in order to be compiled.  
+	api.use("less");
+
+	// Tells meteor to use styles defined in rc:core-theme (identical to rc:default-theme)
+	api.use("reactioncommerce:core-theme@2.0.0");
+
+	api.addFiles("client/main.less", "client");
+	api.addFiles("client/styles/variables.less", "client", {isImport: true});
+  api.addFiles("client/styles/base.less", "client", {isImport: true});
+
 });
