b324c7d94066a784b3ef09ffd1e1b5d2b2fa700d
dashielms <dashielms@gmail.com>
2016-06-03 05:06:17 -0400

    fixed custom packages dirs being ignored by git (added inverse ignore rules to packages/.gitignore)

---
+++ packages/head-fresh/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+	name: "head-fresh:head-fresh",
+	summary: "Head Fresh Custom Store",
+	version: "0.1.0"
+});
+
+Package.onUse(function (api) {
+	api.versionsFrom("METEOR@1.3");
+});
