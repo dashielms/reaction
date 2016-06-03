b4c8a62ba3896b4b836f505da33d3156a6ea8248
dashielms <dashielms@gmail.com>
2016-06-03 11:03:04 -0400

    basic navbar styling

--- packages/hf-freshstore/package.js
+++ packages/hf-freshstore/package.js
@@ -14,8 +14,9 @@ Package.onUse(function (api) {
 	// Tells meteor to use styles defined in rc:core-theme (identical to rc:default-theme)
 	api.use("reactioncommerce:core-theme@2.0.0");
 
-	api.addFiles("client/main.less", "client");
-	api.addFiles("client/styles/variables.less", "client", {isImport: true});
-  api.addFiles("client/styles/base.less", "client", {isImport: true});
+	// TODO: Paths should be only on the client (i.e. client/styles/variables.less, etc)
+	api.addFiles("styles/variables.less", "client", {isImport: true});
+  api.addFiles("styles/base.less", "client", {isImport: true});
+	api.addFiles("main.less", "client");
 
 });
